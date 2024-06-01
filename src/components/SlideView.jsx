1dfd26ad655783c316701c54ad001e8d67e3a0a6
Caitlin Kunchur <crkunchur@gmail.com>
2024-06-01 11:44:07 -0700

    added mouse into slide view

--- src/components/SlideView.jsx
+++ src/components/SlideView.jsx
@@ -37,50 +37,62 @@ export default function SlideView() {
 
   return (
     <Stack spacing={2} sx={{ marginTop: 4, alignItems: 'center' }}>
-    <Typography variant="h6">
-      Part {currentSlide + 1} of {imageArray.length}
-    </Typography>
-    <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', position: 'relative' }}>
-      <Button onClick={handlePrev} sx={{ position: 'absolute', left: 0, color: 'black', fontSize: '48px'  }} disabled={currentSlide === 0}
->
-        <KeyboardArrowLeftIcon fontSize="inherit" />
-      </Button>
-  
-      <Card sx={{ width: '60%', cursor: 'pointer', minHeight: '400px' }} onClick={handleFlip}>
-        {flipped[currentSlide] ? (
-          <CardContent sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-            <LightbulbIcon sx={{ marginBottom: '8px' }} />
-            <Typography variant="body2" color="textSecondary" align="center">
-              {imageArray[currentSlide].longerCaption}
-            </Typography>
-          </CardContent>
-        ) : (
-          <CardMedia
-            component="img"
-            sx={{ height: '400px' }}
-            image={imageArray[currentSlide].src}
-            alt={imageArray[currentSlide].caption}
-          />
-        )}
-      </Card>
-  
-      <Button 
-      onClick={handleNext} 
-      disabled={currentSlide === imageArray.length - 1}
-      sx={{ position: 'absolute', right: 0, color: 'black', fontSize: '48px'  }}>
-        <KeyboardArrowRightIcon fontSize="inherit" />
-      </Button>
-    </Box>
-    {!flipped[currentSlide] && (
-      <Typography variant="body2" color="textSecondary" align="center" sx={{ width: '80%', marginTop: 2 }}>
-        {imageArray[currentSlide].caption}
+      <Typography variant="h6">
+        Part {currentSlide + 1} of {imageArray.length}
       </Typography>
-    )}
-    <Button variant="contained" onClick={navigateToGridView} sx={{ color: 'white', backgroundColor: 'black', borderRadius: '20px', textTransform: 'none', fontSize: '14px' }}>
-    <GridViewIcon/> Switch to Grid View
-    </Button>
-  </Stack>
-  
+      <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', position: 'relative' }}>
+        <Button onClick={handlePrev} sx={{ position: 'absolute', left: 0, color: 'black', fontSize: '48px' }} disabled={currentSlide === 0}
+        >
+          <KeyboardArrowLeftIcon fontSize="inherit" />
+        </Button>
+
+        <Card sx={{ width: '60%', cursor: 'pointer', minHeight: '400px' }} onClick={handleFlip}>
+          {flipped[currentSlide] ? (
+            <CardContent sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+              <LightbulbIcon sx={{ marginBottom: '8px' }} />
+              <Typography variant="body2" color="textSecondary" align="center">
+                {imageArray[currentSlide].longerCaption}
+              </Typography>
+            </CardContent>
+          ) : (
+            <CardMedia
+              component="img"
+              sx={{ height: '400px' }}
+              image={imageArray[currentSlide].src}
+              alt={imageArray[currentSlide].caption}
+            />
+          )}
+        </Card>
+
+        <Button
+          onClick={handleNext}
+          disabled={currentSlide === imageArray.length - 1}
+          sx={{ position: 'absolute', right: 0, color: 'black', fontSize: '48px' }}>
+          <KeyboardArrowRightIcon fontSize="inherit" />
+        </Button>
+      </Box>
+      {!flipped[currentSlide] && (
+        <Typography variant="body2" color="textSecondary" align="center" sx={{ width: '80%', marginTop: 2 }}>
+          {imageArray[currentSlide].caption}
+        </Typography>
+      )}
+      <Button variant="contained" onClick={navigateToGridView} sx={{ color: 'white', backgroundColor: 'black', borderRadius: '20px', textTransform: 'none', fontSize: '14px' }}>
+        <GridViewIcon /> Switch to Grid View
+      </Button>
+
+      <Box
+        sx={{
+          position: 'absolute',
+          bottom: 16,
+          left: 0,
+          width: '20%', // Adjust the width to scale the image
+          height: 'auto' // Maintain aspect ratio
+        }}
+      >
+        <img src={'mouse-speech-bubble.png'} alt="image of blue cartoon mouse in bottom left of screen" style={{ marginLeft: '8px', width: '100%' }} />
+      </Box>
+    </Stack>
+
 
   );
 };
